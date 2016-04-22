c0d266bec2080c752b0a2b21ba5e71568e9b30e3
mbd-s <michaelbsilverberg@gmail.com>
2016-04-22 14:24:24 -0700

    all argument tests pass

--- drills.rb
+++ drills.rb
@@ -10,7 +10,7 @@
 #echo
   # returns the input string
   def echo(input)
-    return input
+    input
   end
 
 #eddie_izzards_height
@@ -18,19 +18,25 @@
   # takes in the height of heels he's wearing (default value: 0)
   # and adds heel height to his base 67 inches for the result
   def eddie_izzards_height(heel_height = 0)
-    return heel_height + 67
+    heel_height + 67
   end
 
 #how_many_args
   # accepts any number of arguments without complaint
   # returns the total number of arguments passed to the function
   # Hint: google ruby *args
+  def how_many_args(*args)
+    args.length
+  end
 
 #find_answer
   # returns the value associated with the 'answer' keyword argument
   # returns nil if it cannot find the 'answer' keyword argument
   # complains when given non-keyword arguments
   # Hint: google ruby keyword arguments
+  def find_answer(args = {})
+    args[:answer]
+  end
 
 ##############################
 #### MANIPULATING STRINGS ####
