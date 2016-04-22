e70359f6ba97c9653c1c37adfc7decab43a2b89b
morgansharif <morgan.sharif@gmail.com>
2016-04-22 14:10:39 -0700

    finished arguments

--- drills.rb
+++ drills.rb
@@ -1,27 +1,37 @@
 #########################
 #### USING ARGUMENTS ####
 #########################
-#say_hello
-  # returns 'hello'
+#says "hello"
+def say_hello
+  "hello"
+end
 
-#echo
-  # returns the input string
+#returns string passed into arg
+def echo(str)
+  p str
+end
 
-#eddie_izzards_height
-  # calculates and returns Eddie Izzard's height 
-  # takes in the height of heels he's wearing (default value: 0)
-  # and adds heel height to his base 67 inches for the result
+# calculates and returns Eddie Izzard's height
+# takes in the height of heels he's wearing (default value: 0)
+# and adds heel height to his base 67 inches for the result
+def eddie_izzards_height(heelHeight=0)
+  heelHeight + 67
+end
 
-#how_many_args
-  # accepts any number of arguments without complaint
-  # returns the total number of arguments passed to the function
-  # Hint: google ruby *args
+# accepts any number of arguments without complaint
+# returns the total number of arguments passed to the function
+# Hint: google ruby *args
+def how_many_args(*arg)
+  arg.length
+end
 
-#find_answer 
-  # returns the value associated with the 'answer' keyword argument
-  # returns nil if it cannot find the 'answer' keyword argument
-  # complains when given non-keyword arguments
-  # Hint: google ruby keyword arguments
+# returns the value associated with the 'answer' keyword argument
+# returns nil if it cannot find the 'answer' keyword argument
+# complains when given non-keyword arguments
+# Hint: google ruby keyword arguments
+def find_answer(h={})
+  h[:answer]
+end
 
 ##############################
 #### MANIPULATING STRINGS ####
