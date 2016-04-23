a3a66d20036b162958ca2768129821651c5ad70c
morgansharif <morgan.sharif@gmail.com>
2016-04-23 16:11:46 -0700

    finished iterators

--- drills.rb
+++ drills.rb
@@ -92,9 +92,19 @@ def seperate(str, separator="-")
 end
 
 ## STRETCH ##
-#croon
-  # seperates word characters with dashes
-  # preserves whitespace between words
+
+# seperates word characters with dashes
+# preserves whitespace between words
+
+# def croon(str) #INCOMPLETE
+#   str.split.each do |word|
+#     word.each_char do |char|
+#       char == " " ? char = " " : res += "#{char}-"
+#     end
+#     word[-1]=""
+#   end
+#   str.join(" ")
+# end
 
 #palindrome_word?
   # determines whether a single word is a palindrome
@@ -126,48 +136,67 @@ end
 ###########################
 #### LOOPS & ITERATORS ####
 ###########################
-#count_spaces
-  # takes in a string
-  # counts the spaces in a string
-  # returns number of spaces
 
-#string_lengths
-  # takes in an array of strings
-  # returns an array containing the lengths of the strings
+# takes in a string
+# counts the spaces in a string
+# returns number of spaces
+def count_spaces(str)
+  str.count(" ")
+end
 
-#remove_falsy_values
-  # takes in a list
-  # filters out falsy values from a list
-  # returns the updated list
+# takes in an array of strings
+# returns an array containing the lengths of the strings
+def string_lengths(arr)
+  arr.map { |e| e.length }
+end
 
-#exclude_last
-  # takes in an array or string
-  # removes the last item from the array or string
-  # returns it
+# takes in a list
+# filters out falsy values from a list
+# returns the updated list
+def remove_falsy_values(arr)
+  arr.reject { |e| !e } # OR arr.reject { |e| !!e }
+end
 
-#exclude_first
-  # takes in an array or string
-  # removes the first item from an array
-  # removes the first character from a string
-  # returns a new string - does not alter the original input (non-destructive)
+# takes in an array or string
+# removes the last item from the array or string
+# returns it
+def exclude_last(arg)
+  arg[0..-2] ## or arg[0...-1]
+end
 
-#exclude_ends
-  # takes in an array or string
-  # removes the first and last items from an array
-  # removes the first and last characters from a string
+# takes in an array or string
+# removes the first item from an array
+# removes the first character from a string
+# returns a new string - does not alter the original input (non-destructive)
+def exclude_first(arg)
+  arg[1..-1]
+end
 
-#select_every_even
-  # takes in an array
-  # returns a list of even-indexed items from the input
+# takes in an array or string
+# removes the first and last items from an array
+# removes the first and last characters from a string
+def exclude_ends(arg)
+  arg[1..-2] #or arg[1...-1]
+end
 
-#select_every_odd
-  # takes in an array
-  # returns a list of odd-indexed items
+# takes in an array
+# returns a list of even-indexed items from the input
+def select_every_even(arr)
+    arr.select.with_index { |e, i| e if i.even? }
+end
 
-#select_every_n
-  # takes in an array
-  # returns a list of items at an index evenly divisible by n
-  # defaults to an n value of 1
+# takes in an array
+# returns a list of odd-indexed items
+def select_every_odd(arr)
+  arr.select.with_index { |e, i|  e if i.odd? }
+end
+
+# takes in an array
+# returns a list of items at an index evenly divisible by n
+# defaults to an n value of 1
+def select_every_n(arr, val=1)
+  arr.select.with_index { |e, i|  e if ( i % val == 0 ) }
+end
 
 ## STRETCH ##
 #compile_agenda
