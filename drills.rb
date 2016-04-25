380e02a710fad8628a34997b0496a61dfa9aabb5
srhbnnstr <srhbnnstr@gmail.com>
2016-04-25 10:09:29 -0700

    finished, most tests passing

--- drills.rb
+++ drills.rb
@@ -46,34 +46,51 @@ end
   # takes in a word
   # returns the first letter of the word
   # lowercases the first letter of the word
+def first_char(string)
+  string[0].downcase
+end
 
 #polly_wanna
   # takes in a word
   # echoes the original word
   # repeats the original word 3 times
   # returns a string with the word repeated
+def polly_wanna(string)
+  string*3
+end
 
 #count_chars
   # takes in a word
   # returns the number of characters in the word
+def count_chars(string)
+  string.length
+end
 
 #yell
   # takes in a message
   # convert the message to uppercase
   # adds an exclamation point to the end of the message
   # returns the message
+def yell(statement)
+  statement.upcase + "!"
+end
 
 ## STRETCH ##
 #to_telegram
   # takes in a message
   # replaces periods with ' STOP'
   # returns the updated message
-
+def to_telegram(statement)
+  statement.gsub(/\./, " STOP")
+end
 
 #spell_out
   # takes in a string
   # converts the string to lowercase
   # returns the input string, with characters seperated by dashes
+def spell_out(str)
+  str.downcase.split("").join("-")
+end
 
 
 #seperate
@@ -81,6 +98,9 @@ end
   # seperates characters with a custom seperator, when supplied with one
   # seperates characters with dashes (by default)
   # returns the modified string
+def seperate(str, seperator="-")
+  str.downcase.split("").join(seperator)
+end
 
 ## STRETCH ##
 #croon
@@ -112,7 +132,9 @@ end
   # does not add a period if one is already there
   # does not add a period if any form of terminal punctuation is present
   # returns the sentence
-
+  def add_period(input)
+    "!?.".include?(input[-1]) ? input : input + "."
+  end
 
 ###########################
 #### LOOPS & ITERATORS ####
@@ -121,44 +143,77 @@ end
   # takes in a string
   # counts the spaces in a string
   # returns number of spaces
+  def count_spaces(string)
+    count = 0
+    space = " "
+    string.each_char do |char|
+      if char == space
+        count += 1
+      end
+    end
+    count
+  end
 
 #string_lengths
   # takes in an array of strings
   # returns an array containing the lengths of the strings
+  def string_lengths(list)
+    list.map {|str| str.length }
+  end
 
 #remove_falsy_values
   # takes in a list
   # filters out falsy values from a list
   # returns the updated list
-
+def remove_falsy_values
+  list.select { |item| !!item }
+end
 #exclude_last
   # takes in an array or string
   # removes the last item from the array or string
   # returns it
+def exclude_last(sequence)
+  sequence[0..-2]
+end
 
 #exclude_first
   # takes in an array or string
   # removes the first item from an array
   # removes the first character from a string
   # returns a new string - does not alter the original input (non-destructive)
+def exclude_first(sequence)
+  sequence[1..-1]
+end
 
 #exclude_ends
   # takes in an array or string
   # removes the first and last items from an array
   # removes the first and last characters from a string
+def exclude_ends(sequence)
+  sequence[1..-2]
+end
 
 #select_every_even
   # takes in an array
   # returns a list of even-indexed items from the input
+def select_every_even(list)
+  list.select.with_index {|item, index| item if index.even? }
+end
 
 #select_every_odd
   # takes in an array
   # returns a list of odd-indexed items
+def select_every_odd(list)
+  list.select.with_index {|item, index| item if index.odd? }
+end
 
 #select_every_n
   # takes in an array
   # returns a list of items at an index evenly divisible by n
   # defaults to an n value of 1
+def select_every_n(list, interval=1)
+  list.select.with_index {|item, index| item if index%interval == 0 }
+end
 
 ## STRETCH ##
 #compile_agenda
@@ -176,6 +231,14 @@ end
   # returns an array containing every integer from 0 to n
   # counts up or down
   # rounds off decimals
+def count_to(n)
+  n = n.to_i
+  if n >= 0
+    (0..n).to_a
+  else
+    0.downto(n).to_a
+  end
+end
 
 #is_integer?
   # takes in a number
@@ -184,7 +247,10 @@ end
   # returns false for non-integer decimals
   # returns false for Float::NAN
   # returns false for non-numbers
-
+def is_integer?(num)
+  num.class == Fixnum || num.class == Bignum ||
+    ( num.is_a?(Float) && !num.nan? && num.to_i == num )
+end
 #is_prime?
   # takes in a number and checks if it's prime
   # returns false for non-integer decimals
@@ -192,12 +258,35 @@ end
   # returns false for numbers divisible by anything but 1 and themselves
   # returns true for prime numbers
   # Hint: google prime numbers!
+def is_prime?(num)
+  if !is_integer?(num) || num <= 1
+    false
+  elsif num <= 1
+    false
+  else
+    (2..(num-1)).each do |n|
+      if num % n == 0
+        return false
+      end
+    end
+    true
+  end
+end
 
 #primes_less_than
   # takes in a number
   # returns an empty array if there are no primes below num
   # does not return the number itself
   # finds all primes less than the given number
+def primes_less_than
+  primes = []
+  (2..num-1).each do |n|
+    if is_prime?(n)
+      primes.push n
+    end
+  end
+  primes
+end
 
 ## STRETCH ##
 #iterative_factorial
@@ -217,6 +306,18 @@ end
   # counts how many times each character appears in a string
   # ignores case
   # returns the hash
+def character_count
+  char_counts = {}
+  sentence.each_char do |char|
+    char = char.downcase
+    if char_counts[char].nil?
+      char_counts[char] = 1
+    else
+      char_counts[char] += 1
+    end
+  end
+  char_counts
+end
 
 ## STRETCH ##
 #word_count
@@ -225,8 +326,23 @@ end
   # ignores case
   # ignores characters that are not in the sequence a-z
   # returns a hash with all the words and their counts
-
+def word_count
+  word_counts = {}
+  sentence.split(" ").each do |word|
+    word = word.downcase.gsub(/[^a-z]/i, "")
+    if word_counts[word].nil?
+      word_counts[word] = 1
+    else
+      word_counts[word] += 1
+    end
+  end
+  word_counts
+end
 ## STRETCH ##
 #most_frequent_word
   # takes in a string
   # finds the word in a string that appears with the most frequency
+def most_frequent_word(sentence)
+  word_counts = word_count(sentence)
+  word_counts.empty? ? nil : word_counts.invert.max[1]
+end
