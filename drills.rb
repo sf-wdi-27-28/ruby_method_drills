ccfc6b23b3ad92a1e67b87d6e66b9bbc48ae0b84
morgansharif <morgan.sharif@gmail.com>
2016-04-24 22:01:30 -0700

    finished numbers

--- drills.rb
+++ drills.rb
@@ -209,33 +209,57 @@ end
 ##############################
 #### MANIPULATING NUMBERS ####
 ##############################
-#count_to
-  # takes in a number
-  # returns an array containing every integer from 0 to n
-  # counts up or down
-  # rounds off decimals
 
-#is_integer?
-  # takes in a number
-  # returns true for Fixnums and Bignums (whole number or 'integer' types)
-  # returns true for Floats (decimals) equal to integers
-  # returns false for non-integer decimals
-  # returns false for Float::NAN
-  # returns false for non-numbers
+# takes in a number
+# returns an array containing every integer from 0 to n
+# counts up or down
+# rounds off decimals
+def count_to(num)
+  num = num.to_i
+  num >= 0 ? [*0..num] : [*num..0].reverse
+end
 
-#is_prime?
-  # takes in a number and checks if it's prime
-  # returns false for non-integer decimals
-  # returns false for numbers less than or equal to 1
-  # returns false for numbers divisible by anything but 1 and themselves
-  # returns true for prime numbers
-  # Hint: google prime numbers!
+# takes in a number
+# returns true for Fixnums and Bignums (whole number or 'integer' types)
+# returns true for Floats (decimals) equal to integers
+# returns false for non-integer decimals
+# returns false for Float::NAN
+# returns false for non-numbers
+def is_integer?(num)
+  # first two arguments can be replaced with num.is_a?(Integer)
+  num.class == Fixnum || num.class == Bignum || ( num.class == Float && !num.nan? && num == num.floor )
+end
 
-#primes_less_than
-  # takes in a number
-  # returns an empty array if there are no primes below num
-  # does not return the number itself
-  # finds all primes less than the given number
+# takes in a number and checks if it's prime
+# returns false for non-integer decimals
+# returns false for numbers less than or equal to 1
+# returns false for numbers divisible by anything but 1 and themselves
+# returns true for prime numbers
+# Hint: google prime numbers!
+def is_prime?(num)
+  if !num.is_a?(Integer) || ( num.class == Float && !num.nan? && num == num.floor ) || num <= 1
+    false
+  else
+    (2...num).each do |i|
+      if num % i == 0
+        return false
+      end
+    end
+    true
+  end
+end
+
+# takes in a number
+# returns an empty array if there are no primes below num
+# does not return the number itself
+# finds all primes less than the given number
+def primes_less_than(num)
+  res = []
+  (2...num).each do |i|
+    res.push(i) if is_prime?(i) == true
+  end
+  res
+end
 
 ## STRETCH ##
 #iterative_factorial
